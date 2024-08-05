786993671c49966c07562c36dac9b6efeb28ac16
Iyana <114933494+yayakixtenet@users.noreply.github.com>
2024-08-05 15:38:59 -0400

    fix auth middleware

--- backend/app.ts
+++ backend/app.ts
@@ -1,18 +1,17 @@
 import express from "express";
 import cors from "cors";
 import userRouter from "./lib/controllers/users/controller";
+import profileRouter from "./lib/controllers/profile/controller";
+import { ClerkExpressRequireAuth } from "@clerk/clerk-sdk-node";
+import "dotenv/config";
 
 const app = express();
 
-app.use(
-	cors({
-		origin: ['origin(s)'],
-		allowedHeaders: ['Authorization', 'Content-Type'],
-	})
-);
+app.use(cors({ allowedHeaders: ["Authorization", "Content-Type"] }));
 
 app.use(express.json());
 
-app.use("/api", userRouter);
+// app.use("/api", userRouter);
+app.use("/api", profileRouter);
 
 export default app;
--- backend/utils/global.d.ts
+++ backend/utils/global.d.ts
@@ -1,11 +1,10 @@
 import express from "express";
+import { User } from "@prisma/client";
 
 declare global {
   namespace Express {
     interface Request {
-      auth: {
-        userId: string;
-      };
+      user?: User;
     }
   }
 }
