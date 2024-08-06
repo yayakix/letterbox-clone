79bb930dc86874b85996be5bd2381f1874984502
pranavpatilsce <ompranav97@gmail.com>
2024-08-06 16:07:50 -0400

    rouer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MovieProfile from "./components/pages/MovieProfile";
 import Navbar from "./components/compound/Navbar";
 
 export default function App() {
   return (
-
-
-    <>
-      <Navbar />
-      {/* <MovieProfile movieId="clzhhaqku000147fatnwla93m" /> */}
-    </>
-
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navbar />} />
+          <Route path="/movie/:movieId" element={<MovieProfile movieId="clzhhaqku000147fatnwla93m" />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
