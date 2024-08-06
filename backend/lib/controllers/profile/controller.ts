36a66871970f77e0c625bb6880701d8753736331
Iyana <114933494+yayakixtenet@users.noreply.github.com>
2024-08-06 16:06:01 -0400

    rename userid thing

--- backend/lib/controllers/profile/controller.ts
+++ backend/lib/controllers/profile/controller.ts
@@ -14,7 +14,7 @@ profileRouter.get("/", async (req, res) => {
     return res.status(401).json({ error: "Unauthorized" });
   }
   try {
-    const profile = await profileClient.getProfile(req.user.id);
+    const profile = await profileClient.getProfile(req.user.userId);
     res.json(profile);
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
@@ -23,11 +23,11 @@ profileRouter.get("/", async (req, res) => {
 
 // Get all comments a user has made
 profileRouter.get("/yaps", async (req, res) => {
-  if (!req.user?.id) {
+  if (!req.user?.userId) {
     return res.status(401).json({ error: "Unauthorized" });
   }
   try {
-    const yaps = await profileClient.getYaps(req.user.id);
+    const yaps = await profileClient.getYaps(req.user.userId);
     res.json(yaps);
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
@@ -37,12 +37,12 @@ profileRouter.get("/yaps", async (req, res) => {
 profileRouter.post("/yaps/:filmId", async (req, res) => {
   console.log(req.body);
   console.log("hit");
-  if (!req.user?.id) {
+  if (!req.user?.userId) {
     return res.status(401).json({ error: "Unauthorized" });
   }
   try {
     const yap = await profileClient.postYap(
-      req.user.id,
+      req.user.userId,
       req.body.content,
       req.params.filmId
     );
@@ -54,11 +54,11 @@ profileRouter.post("/yaps/:filmId", async (req, res) => {
 
 // get all films a user has liked
 profileRouter.get("/liked", async (req, res) => {
-  if (!req.user?.id) {
+  if (!req.user?.userId) {
     return res.status(401).json({ error: "Unauthorized" });
   }
   try {
-    const likedFilms = await profileClient.getLikedFilms(req.user.id);
+    const likedFilms = await profileClient.getLikedFilms(req.user.userId);
     res.json(likedFilms);
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
@@ -67,12 +67,13 @@ profileRouter.get("/liked", async (req, res) => {
 
 // like a film
 profileRouter.post("/liked/:filmId", async (req, res) => {
-  if (!req.user?.id) {
+  if (!req.user?.userId) {
     return res.status(401).json({ error: "Unauthorized" });
   }
+  console.log("filmid:", req.params.filmId);
   try {
     const likedFilm = await profileClient.addFilmToLiked(
-      req.user.id,
+      req.user.userId,
       req.params.filmId
     );
     res.json(likedFilm);
@@ -82,11 +83,11 @@ profileRouter.post("/liked/:filmId", async (req, res) => {
 });
 
 profileRouter.get("/following", async (req, res) => {
-  if (!req.user?.id) {
+  if (!req.user?.userId) {
     return res.status(401).json({ error: "Unauthorized" });
   }
   try {
-    const following = await profileClient.getFollowing(req.user.id);
+    const following = await profileClient.getFollowing(req.user.userId);
     res.json(following);
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
@@ -94,11 +95,11 @@ profileRouter.get("/following", async (req, res) => {
 });
 
 profileRouter.get("/followers", async (req, res) => {
-  if (!req.user?.id) {
+  if (!req.user?.userId) {
     return res.status(401).json({ error: "Unauthorized" });
   }
   try {
-    const followers = await profileClient.getFollowers(req.user.id);
+    const followers = await profileClient.getFollowers(req.user.userId);
     res.json(followers);
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
@@ -107,11 +108,11 @@ profileRouter.get("/followers", async (req, res) => {
 
 // get all films a user has watched
 profileRouter.get("/watched", async (req, res) => {
-  if (!req.user?.id) {
+  if (!req.user?.userId) {
     return res.status(401).json({ error: "Unauthorized" });
   }
   try {
-    const watchedFilms = await profileClient.getWatchedFilms(req.user.id);
+    const watchedFilms = await profileClient.getWatchedFilms(req.user.userId);
     res.json(watchedFilms);
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
@@ -120,12 +121,12 @@ profileRouter.get("/watched", async (req, res) => {
 
 // watch a film
 profileRouter.post("/watched/:filmId", async (req, res) => {
-  if (!req.user?.id) {
+  if (!req.user?.userId) {
     return res.status(401).json({ error: "Unauthorized" });
   }
   try {
     const watchedFilm = await profileClient.addFilmToWatched(
-      req.user.id,
+      req.user.userId,
       req.params.filmId
     );
     res.json(watchedFilm);
