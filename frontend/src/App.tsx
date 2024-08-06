93c037545655d1f97f71b1c8a1b1f7d515b6c055
pranavpatilsce <ompranav97@gmail.com>
2024-08-06 17:51:38 -0400

    router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,27 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import MovieProfile from "./components/pages/MovieProfile";
-import Navbar from "./components/compound/Navbar";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import MovieProfile from "./components/pages/MovieProfile.tsx";
+import Home from "./components/pages/Home.tsx";
+import UserProfile from "./components/pages/UserProfile.tsx";
+import { useAuth } from "@clerk/clerk-react";
+import Navbar from "./components/compound/Navbar.tsx";
 
 export default function App() {
+
+  const { sessionId, actor, isSignedIn, isLoaded } = useAuth();
+
+  console.log('isSignedIn', { sessionId, actor, isSignedIn, isLoaded })
+
   return (
     <div>
-      <Router>
+      <BrowserRouter>
+        <Navbar/>
         <Routes>
-          <Route path="/" element={<Navbar />} />
-          <Route path="/movie/:movieId" element={<MovieProfile movieId="clzhhaqku000147fatnwla93m" />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:id" element={<MovieProfile movieId="clzhhaqku000147fatnwla93m" />} />
+          <Route path="/profile" element={<UserProfile />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </div>
   )
 }
