7b3870beb62b0f002f59b46dfba98dd45c281648
pranavpatilsce <ompranav97@gmail.com>
2024-08-07 16:54:57 -0400

    pause

--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -1,7 +1,27 @@
+import React from "react";
+
+
 export default function Home() {
 	return (
-		<div className="flex flex-col w-full h-screen bg-blue-500">
-			<h1>Home</h1>
+		<div className="flex flex-col w-full h-screen items-center bg-transparent">
+			<div className="flex flex-row items-center justify-evenly w-8/12">
+				<div>
+					Filters
+				</div>
+				<div>
+					Search Box
+				</div>
+			</div>
+			<div className="flex flex-col items-center h-screen w-8/12">
+				<h1>popular movies</h1>
+				<div className="flex flex-row">
+					<div className="w-1/4 flex flex-col items-center">
+						<img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
+						<h1>Movie Title</h1>
+					</div>
+
+				</div>
+			</div>
 		</div>
 	);
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,11 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    daisyui: {
+      themes: ["night"],
+    },
   },
-  plugins: [],
+  plugins: [
+    require("daisyui"),
+  ],
 };
